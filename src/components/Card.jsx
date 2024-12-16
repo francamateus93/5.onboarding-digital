a8cf34890b1b8534fa80fadcf5b723f01c60a40a
francamateus93 <francamateus@gmail.com>
2024-12-16 21:53:35 +0100

    Exercise 3 - Completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-function Card({title, description, background, onNext}) {
+function Card({title, description, image, backgroundColor, onNext}) {
   return (
+    <>
     <div className="container">
-      <img src={background} alt="img-card" />
-      <h2 className="title-card">{title}</h2>
-      <p className="description-card">{description}</p>
-      <button className="btn-card" onClick={onNext}>&#8594;</button>
+      <div className="image-container" style={{backgroundColor: backgroundColor}}>
+        <img src={image} alt="img-card" />
+      </div>
+      <div className="text-container">
+        <h2 className="title-card">{title}</h2>
+        <p className="description-card">{description}</p>
+      <div className="btn-container">
+        <button className="btn-card" onClick={onNext}>&#8594;</button>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
 

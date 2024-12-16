65bfc1963ac2ff8338fee331e5849b3d1d791914
francamateus93 <francamateus@gmail.com>
2024-12-16 12:13:28 +0100

    Exercise 2 - Completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,27 @@ import './App.css'
 
 function App() {
 
-  //Array con datos
     const tutorialData = [
       { title: "Dedica moltes hores", description: "Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaàs una millora ràpidament." },
       { title: "Programa projectes propis", description: "Més val 10 hores treballant en projectes propris, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge." },
       { title: "Procura descansar", description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Millotatàs la teva concentració i consolidaràs el teu aprenentatge." },
     ];
 
-    // Contador para el status atual
+    // Actualiza el status
     const [step, setStep] = useState(0);
     const data = tutorialData[step];
 
+    // Incrementa el Step
+    const nextStep = () => {
+      setStep(step => step + 1)
+    }
+    
   return (
     <div>
       <Card
       title={data.title}
-      description={data.description}/>
+      description={data.description}
+      onNext={nextStep}/>
     </div>
   )
 }

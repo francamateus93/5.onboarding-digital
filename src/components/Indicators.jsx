634744810f639b4b90b266149cc03c616fd54251
francamateus93 <francamateus@gmail.com>
2024-12-17 19:12:01 +0100

    Exercise 5 - Completed

--- src/components/Indicators.jsx
+++ src/components/Indicators.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import styled from "styled-components"
+
+const IndicatorsContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  justify-content: start;
+`;
+
+const Indicator = styled.div`
+  width: 0.8rem;
+  height: 0.8rem;
+  border-radius: 50%;
+  background-color: ${(props) => (props.active ? "#2a2a2a" : "#999")};
+  transition: background-color 0.5s ease;
+`;
 
 function Indicators({tutorialData, step}) {
   return (
-    <div className="indicators">
+    <IndicatorsContainer>
       {tutorialData.map((data) => (
-      <div key={data.id} className={`indicator ${data.id - 1 === step ? "active" : ""}`}>
-      </div>
+        <Indicator key={data.id} active={data.id - 1 === step} />
     ))}
-    </div>
+    </IndicatorsContainer>
   )
 }
 
